13be5731d348399f192661a63417583b5befb9d6
skplanet <skplanet@10.203.207.121>
2016-07-21 22:05:10 +0900

    8번, countOfAnswer 1증가. AJAX자바스크립트 처리는 추후에..

--- src/main/java/next/controller/qna/AddAnswerController.java
+++ src/main/java/next/controller/qna/AddAnswerController.java
@@ -3,6 +3,8 @@ package next.controller.qna;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import next.dao.QuestionDao;
+import next.model.Question;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +17,7 @@ public class AddAnswerController extends AbstractController {
 	private static final Logger log = LoggerFactory.getLogger(AddAnswerController.class);
 
 	private AnswerDao answerDao = new AnswerDao();
+	private QuestionDao questionDao = new QuestionDao();
 
 	@Override
 	public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
@@ -22,8 +25,10 @@ public class AddAnswerController extends AbstractController {
 				req.getParameter("contents"), 
 				Long.parseLong(req.getParameter("questionId")));
 		log.debug("answer : {}", answer);
-		
+
 		Answer savedAnswer = answerDao.insert(answer);
+
+		questionDao.updateCountOfAnswer(answer.getQuestionId(), questionDao.findById(answer.getQuestionId()).getCountOfComment() + 1);
 		return jsonView().addObject("answer", savedAnswer);
 	}
 }
--- src/main/java/next/dao/QuestionDao.java
+++ src/main/java/next/dao/QuestionDao.java
@@ -56,4 +56,13 @@ public class QuestionDao {
 		
 		return jdbcTemplate.queryForObject(sql, rm, questionId);
 	}
+
+	public void updateCountOfAnswer(long questionId, int countOfAnswer) {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+		String sql = "UPDATE QUESTIONS SET countOfAnswer = ? WHERE questionId = ?";
+		jdbcTemplate.update(sql,
+				countOfAnswer,
+				questionId
+		);
+	}
 }
