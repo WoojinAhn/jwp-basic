315b8c3f43842f18ec7a2aff5fb7ad06e2f1048a
skplanet <skplanet@10.203.207.121>
2016-07-28 22:14:22 +0900

    요구사항3. 메소드 단위 테스트 구현 (by @MyTest 검출)

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,22 @@
 package core.ref;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class Junit4TestRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(Junit4TestRunner.class);
+
 	@Test
 	public void run() throws Exception {
 		Class<Junit4Test> clazz = Junit4Test.class;
-
+		for(Method method : clazz.getDeclaredMethods()) {
+			logger.debug("method name : " + method.getName());
+			if(method.isAnnotationPresent(MyTest.class))
+				method.invoke(clazz.newInstance());
+		}
 	}
 }
