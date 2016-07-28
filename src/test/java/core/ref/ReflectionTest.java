165d4eac7836ac193bc4bbe125b6418badc33122
skplanet <skplanet@10.203.207.121>
2016-07-28 21:56:57 +0900

    요구사항1. 필드/메소드/생성자 출력

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -6,6 +6,10 @@ import org.slf4j.LoggerFactory;
 
 import next.model.Question;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class ReflectionTest {
 	private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 	
@@ -13,5 +17,11 @@ public class ReflectionTest {
 	public void showClass() {
 		Class<Question> clazz = Question.class;
 		logger.debug(clazz.getName());
+		logger.debug("######## Fields ##########");
+		for (Field field : clazz.getDeclaredFields()) {	logger.debug(field.getName()); }
+		logger.debug("######## Methods #########");
+		for (Method method : clazz.getDeclaredMethods()) {	logger.debug(method.getName()); }
+		logger.debug("##### Constructors #######");
+		for (Constructor constructor : clazz.getDeclaredConstructors()) {	logger.debug(constructor.getName()); }
 	}
 }
