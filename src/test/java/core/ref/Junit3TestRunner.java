97f04074aaf21c148b4d53024332efce0ee0565c
skplanet <skplanet@10.203.207.121>
2016-07-28 22:09:54 +0900

    요구사항2. 메소드 단위 테스트 구현 (by 클래스내메소드리스트 올 소환해서)

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -1,11 +1,21 @@
 package core.ref;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
 
 public class Junit3TestRunner {
+	private static final Logger logger = LoggerFactory.getLogger(Junit3TestRunner.class);
+
 	@Test
 	public void run() throws Exception {
 		Class<Junit3Test> clazz = Junit3Test.class;
+		for(Method method : clazz.getDeclaredMethods()) {
+			logger.debug("method name : " + method.getName());
+			method.invoke(clazz.newInstance());
+		}
 
 	}
 }
